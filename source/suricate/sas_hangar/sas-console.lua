61c4999e0eb534e37b6741e657f4af12a714f3b3
zorax4295 <108086156+zorax4295@users.noreply.github.com>
2026-05-10 19:05:08 +0200

    add: ajout de plein d'element varié a la page sasControl

--- source/suricate/sas_hangar/sas-console.lua
+++ source/suricate/sas_hangar/sas-console.lua
@@ -7,6 +7,13 @@
 ------------------------------------------------------------------------------------------------
 
 
+----------------------------
+-- import de la librairie
+----------------------------
+
+local scriptedScreen= require("scriptedScreen")
+
+
 
 -- Déffinition des parametre de chaque écran
 local function createScreen(name)
@@ -32,7 +39,8 @@ end
 --Liste toute les page 
 local ui = {
     accueil = createScreen("accueil"),
-    sasControl = createScreen("sasControl")
+    sasControl = createScreen("sasControl"),
+    setting = createScreen("setting"),
 }
 
 ------------------------
@@ -52,22 +60,194 @@ ui.sasControl.clear()
 ui.accueil.set()
 ui.sasControl.set()
 
---Liste tout les elements créer dans chaque écran
+--Permet l'imbriquation d'elements dans des container
 local container = {
-    accueil = {
-        menu = ui.accueil.surface:element({
-            id = "container_menu_auto", type = "panel",
-            rect = { unit = "px", x = 0, y = 0, w = 540, h = 40 },
+    sasControl = {
+        menu = ui.sasControl.surface:element({
+            id = "container_menu_sasControl", type = "panel",
+            rect = { unit = "px", x = 0, y = 410, w = w, h = 50 },
+            style = { bg = "#00000000" }
+        }),
+        run = ui.sasControl.surface:element({
+            id = "container_run_sasControl", type = "panel",
+            rect = { unit = "px", x = 0, y = 108, w = w, h = 215 },
             style = { bg = "#00000000" }
         }),
     },
 }
+--Contien des sous container imbriqué dans des container
+local subContainer = {
+    sasControl = {
+        run = {
+            title = container.sasControl.run:element({
+                id = "container_run_title_sasControl", type = "panel",
+                rect = { unit = "px", x = 0, y = 0, w = w, h = 50 },
+                style = { bg = "#00000000" }
+            }),
+            info = container.sasControl.run:element({
+                id = "container_run_info_sasControl", type = "panel",
+                rect = { unit = "px", x = 0, y = 50, w = w, h = 115 },
+                style = { bg = "#00000000" }
+            }),
+        },
+    },
+}
+--Liste tout les elements créer dans chaque écran
 local element = {
     cycleSas = {
+        background = ui.sasControl.surface:element({
+            id = "background", type = "panel",
+            rect = { unit = "px", x = 0, y = 0, w = w, h = h },
+            style = { bg = "#000000" }
+        }),
         weatherPanel = ui.sasControl.surface:element({
             id = "weatherPanel", type = "image",
             rect = { unit = "px", x = 0, y = 0, w = w, h = 108 },
             props = { url = "https://raw.githubusercontent.com/zorax4295-organization/Galacticon/refs/heads/suricate/sas/hangar/source/.ressource/sas_hangar_vehiculaire/no_storm.png" },
         }),
+        menu = {
+            buttonSetting = {
+                image = container.sasControl.menu:element({
+                    id = "imageButtonSetting_sasControl", type = "image",
+                    rect = { unit = "px", x = 311, y = 0, w = 149, h = 50 },
+                    props = { url = "https://raw.githubusercontent.com/zorax4295-organization/Galacticon/refs/heads/suricate/sas/hangar/source/.ressource/sas_hangar_vehiculaire/button_setting_blue.png" },
+                }),
+                button = container.sasControl.menu:element({
+                    id = "buttonSetting_sasControl", type = "button",
+                    rect = { unit = "px", x = 311, y = 0, w = 149, h = 50 },
+                    props = { text = "" },
+                    style = { bg = "#00000000", text = "#FFFFFF", font_size = 14 },
+                    on_click = function()
+                        ui.setting.set()
+                    end
+                }),
+            },
+            buttonMenu = {
+                image = container.sasControl.menu:element({
+                    id = "imageButtonMenu_sasControl", type = "image",
+                    rect = { unit = "px", x = 0, y = 0, w = 57, h = 50 },
+                    props = { url = "https://raw.githubusercontent.com/zorax4295-organization/Galacticon/refs/heads/suricate/sas/hangar/source/.ressource/sas_hangar_vehiculaire/menu_blue.png" },
+                }),
+                button = container.sasControl.menu:element({
+                    id = "buttonMenu_sasControl", type = "button",
+                    rect = { unit = "px", x = 0, y = 0, w = 57, h = 50 },
+                    props = { text = "" },
+                    style = { bg = "#00000000", text = "#FFFFFF", font_size = 14 },
+                    on_click = function()
+                    end
+                }),
+            },
+        },
+        buttonCycle = {
+            image = ui.sasControl.surface:element({
+                id = "imageButtonCycle_sasControl", type = "image",
+                rect = { unit = "px", x = 76, y = 337, w = 307, h = 60 },
+                props = { url = "https://raw.githubusercontent.com/zorax4295-organization/Galacticon/refs/heads/suricate/sas/hangar/source/.ressource/sas_hangar_vehiculaire/button_start.png" },
+            }),
+            button = ui.sasControl.surface:element({
+                id = "buttonCycle_sasControl", type = "button",
+                rect = { unit = "px", x = 76, y = 337, w = 307, h = 60 },
+                props = { text = "" },
+                style = { bg = "#00000000", text = "#FFFFFF", font_size = 14 },
+                on_click = function()
+                end
+            }),
+        },
+        run = {
+            title = {
+                labelTitle = subContainer.sasControl.run.title:element({
+                    id = "title_sasControl", type = "label",
+                    rect = { unit = "px", x = 204, y = 4, w = 51, h = 20 },
+                    props = { text = "SAS" },
+                    style = { font_size = 24, color = "#0bc1f4", align = "center" }
+                }),
+                labelSubTitle = subContainer.sasControl.run.title:element({
+                    id = "subTitle_sasControl", type = "label",
+                    rect = { unit = "px", x = 133, y = 27, w = 193, h = 17 },
+                    props = { text = "Interface de fonctionnement" },
+                    style = { font_size = 14, color = "#FFFFFF", align = "center" }
+                }),
+                border = scriptedScreen.element.createRect(subContainer.sasControl.run.title, "borderTitle_sasControl",
+                subContainer.sasControl.run.title.rect.x,
+                subContainer.sasControl.run.title.rect.y,
+                w, 50, "#0bc1f4", 2),
+            },
+            info = {
+                room = {
+                    labelTitleRoom = subContainer.sasControl.run.info:element({
+                        id = "labelTitleRoom_sasControl", type = "label",
+                        rect = { unit = "px", x = 18, y = 3, w = 158, h = 19 },
+                        props = { text = "Pression dans la pièce" },
+                        style = { font_size = 12, color = "#FFFFFF", align = "center" }
+                    }),
+                    labelStateRoom = subContainer.sasControl.run.info:element({
+                        id = "labelStateRoom_sasControl", type = "label",
+                        rect = { unit = "px", x = 56, y = 90, w = 82, h = 20 },
+                        props = { text = "NORMAL" },
+                        style = { font_size = 14, color = "#008000 ", align = "center" }
+                    }),
+                    gaugePressureRoom = subContainer.sasControl.run.info:element({
+                        id = "gaugePressureRoom_sasControl", type = "gauge",
+                        rect = { unit = "px", x = 56, y = 25, w = 80, h = 60 },
+                        props = {
+                            value = 0,
+                            min = 0,
+                            max = 300,
+                            warn = 140/300,
+                            danger = 170/300,
+                            label = "",
+                            unit = " kPa",
+                        },
+                        style = {
+                            bg = "#111827",
+                            arc_thickness = 8,
+                            font_size = 12,
+                            value_color = "#0bc1f4",
+                            label_color = "#FFFFFF",
+                        }
+                    }),
+
+
+                    labelTitleTank = subContainer.sasControl.run.info:element({
+                        id = "labelTitleTank_sasControl", type = "label",
+                        rect = { unit = "px", x = 287, y = 5, w = 115, h = 17 },
+                        props = { text = "Pression dans le réservoire du sas" },
+                        style = { font_size = 12, color = "#FFFFFF", align = "center" }
+                    }),
+                    labelStateTank = subContainer.sasControl.run.info:element({
+                        id = "labelStateTank_sasControl", type = "label",
+                        rect = { unit = "px", x = 304, y = 90, w = 82, h = 20 },
+                        props = { text = "NORMAL" },
+                        style = { font_size = 14, color = "#008000 ", align = "center" }
+                    }),
+                    gaugePressureTank = subContainer.sasControl.run.info:element({
+                        id = "gaugePressureTank_sasControl", type = "gauge",
+                        rect = { unit = "px", x = 305, y = 28, w = 80, h = 60 },
+                        props = {
+                            value = 0,
+                            min = 0,
+                            max = 60000,
+                            warn = 45/60,
+                            danger = 50/60,
+                            label = "",
+                            unit = " kPa",
+                        },
+                        style = {
+                            bg = "#111827",
+                            arc_thickness = 8,
+                            font_size = 12,
+                            value_color = "#0bc1f4",
+                            label_color = "#FFFFFF",
+                        }
+                    }),
+                },
+            },
+            -- a modifier par une image
+            state = container.sasControl.run:element({
+                id = "temp_state", type = "panel",
+                rect = { unit = "px", x = 0, y = 165, w = w, h = 50 },
+                style = { bg = "#D9D9D9" }
+            }),
+        },
     },
 }
