d2e4223c624cee617dd14f324fe9f696b11d8d37
zorax4295 <108086156+zorax4295@users.noreply.github.com>
2026-05-30 22:53:28 +0200

    Finalisation du programme

--- source/zayt/Code_serveur_Door.lua
+++ source/zayt/Code_serveur_Door.lua
@@ -22,8 +22,8 @@ local ventInterId = ic.find("Powered Vent inter")
 local ventExterId = ic.find("Powered Vent exter")
 local alarmId = ic.find("Logic Alarm")
 local numPadId = ic.find("Logic Num Pad")
-local display1Id = ic.find("Display 1")
-local display2Id = ic.find("Display 2")
+local displayState = ic.find("Display state")
+local displayPressure = ic.find("Display pressure")
 local flashLightHash = hash("StructureFlashingLight")
 local lightHash = hash("StructureLightLongWide")
 
@@ -44,34 +44,55 @@ local sensCycle = {
     depressurizing = 1,
 }
 local actualSensCycle = sensCycle.depressurizing
+local displayStateValue = {
+    cycleInProgress = pack_ascii6("Cycle"),
+    idle = pack_ascii6("Idle"),
+}
+
 
 ----------------------------
 -- Définition des functions
 ----------------------------
 
+local function refreshPressureDisplay()
+    --Pression en kPa
+    local pressureRoom = system.safe.readId(gasSensorId, LT.Pressure, "Gas Sensor Interieur") * 1000 -- lit et convertie de kPa en Pa
+    system.safe.writeId(displayPressure, LT.Setting, pressureRoom, "Led Display Pressure")
+    if pressureRoom >= 240000 then
+        system.safe.writeId(displayPressure, LT.Color, 4) --Rouge
+    elseif pressureRoom >= 150000 then
+        system.safe.writeId(displayPressure, LT.Color, 3) --Orange
+    else
+        system.safe.writeId(displayPressure, LT.Color, 2) --Vert
+    end
+end
 local function pressurizing()
     actualSensCycle = sensCycle.depressurizing
-    print(system.log.time() .. system.log.level("info") .. " : ServerDoor pressurizing" )
+    print(system.log.time() .. system.log.level("info") .. " : Server Room Pressurizing Started" )
+    system.safe.writeId(displayState, LT.Setting, displayStateValue.cycleInProgress, "Led Display State")
     ic.batch_write(flashLightHash, LT.On, 1)
     ic.batch_write(lightHash, LT.On, 0)
     system.safe.writeId(alarmId, LT.On, 1, "Alarm")
-    system.safe.writeId(ventInterId, LT.Mode, 0, "Vent Interieur")
-    system.safe.writeId(ventInterId, LT.On, 1, "Vent Interieur")
+    yield()
+    system.safe.writeId(ventInterId, LT.Mode, 0, "Vent Exterieur")
+    system.safe.writeId(ventInterId, LT.On, 1, "Vent Exterieur")
 
-    while not system.safe.readId(gasSensorId, LT.Pressure, "Gas Sensor Interieur") >= system.safe.readId(gasSensorExterId, LT.Pressure, "Gas Sensor Exterieur") do
+    while system.safe.readId(gasSensorId, LT.Pressure, "Gas Sensor Interieur") <= system.safe.readId(gasSensorExterId, LT.Pressure, "Gas Sensor Exterieur") do
         yield()
     end
 
-    system.safe.writeId(ventInterId, LT.On, 0, "Vent Interieur")
+    system.safe.writeId(ventInterId, LT.On, 0, "Vent Exterieur")
+    yield()
+    system.safe.writeId(doorId, LT.Open, 1, "Blast Door") --Ouvert
     ic.batch_write(flashLightHash, LT.On, 0)
     ic.batch_write(lightHash, LT.On, 1)
     system.safe.writeId(alarmId, LT.On, 0, "Alarm")
-    yield()
-    system.safe.writeId(doorId, LT.Open, 1, "Blast Door") --ouverte
+    print(system.log.time() .. system.log.level("info") .. " : Server Room Pressurizing Finished" )
 end
 local function depressurizing()
     actualSensCycle = sensCycle.pressurizing
     print(system.log.time() .. system.log.level("info") .. " : Server Room depressurizing Started" )
+    system.safe.writeId(displayState, LT.Setting, displayStateValue.cycleInProgress, "Led Display State")
     system.safe.writeId(doorId, LT.Open, 0, "Blast Door") --fermé
     ic.batch_write(flashLightHash, LT.On, 1)
     ic.batch_write(lightHash, LT.On, 0)
@@ -84,6 +105,7 @@ local function depressurizing()
         yield()
     end
 
+    sleep(2)
     system.safe.writeId(ventExterId, LT.On, 0, "Vent Exterieur")
     ic.batch_write(flashLightHash, LT.On, 0)
     ic.batch_write(lightHash, LT.On, 1)
@@ -95,15 +117,34 @@ end
 ----------------------------
 -- Init du system
 ----------------------------
+
+system.safe.writeId(ventExterId, LT.On, 0, "Vent Exterieur")
+system.safe.writeId(ventInterId, LT.On, 0, "Vent Interieur")
+ic.batch_write(flashLightHash, LT.On, 0)
+ic.batch_write(lightHash, LT.On, 1)
+system.safe.writeId(alarmId, LT.Color , 4)
+system.safe.writeId(displayState, LT.Mode, 10, "Led Display State") --Définition du led display en mode string
+system.safe.writeId(displayPressure, LT.Mode, 14, "Led Display Pressure") --Définition du led display en mode pressure
+
+
+function tick(dt) --S'execute a chaque tick du jeux
+    refreshPressureDisplay()
+end
+
+
+
 while true do
     local actualAccessLevel = system.safe.read(housingAccess, LT.Setting, "card")
     local actualCode = system.safe.read(accessCode, LT.Setting, "keypad")
     local dcy = system.safe.readId(buttonDepartCycleId, LT.Activate, "Button Depart Cycle")
+    system.safe.writeId(displayState, LT.Setting, displayStateValue.idle, "Led Display State")
 
     if actualCode == code and (actualAccessLevel == accessLevel.granted or actualAccessLevel == accessLevel.maintenance) and dcy == 1 then
         print(system.log.time() .. system.log.level("info") .. " : ServerDoor Access " .. system.utils.color("Green", "Granted"))
         if actualSensCycle == sensCycle.depressurizing then
             depressurizing()
+        else
+            pressurizing()
         end
     elseif dcy == 1 then
         print(system.log.time() .. system.log.level("info") .. " : ServerDoor Access " .. system.utils.color("Red","Denied"))
