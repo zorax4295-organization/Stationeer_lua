4ab2c7b64866e8d6bbe312f66df2499178bb7a94
zayt <frederic.bohet@gmail.com>
2026-06-12 19:00:14 +0200

    Fin du programme, programme fonctionnel
    Co-authored-by: zorax4295

--- source/zayt/Centrifuges/Centrifuge_Controle_Vidange.lua
+++ source/zayt/Centrifuges/Centrifuge_Controle_Vidange.lua
@@ -1,4 +1,30 @@
 
+----------------------------
+-- import de la librairie
+----------------------------
+
+local system = require("system")
+
+----------------------------
+-- Définition des données
+----------------------------
+
+local LT = ic.enums.LogicType
+local LST = ic.enums.LogicSlotType
+
+
+
+ic.net.listen("centrifuge/vidange", function(fromId, fromName, payload)
+    if type(payload) ~= "table" then
+        print(system.log.time() .. "h " .. system.log.level("warn") .. " : La puce " .. system.utils.color("Yellow", fromName) .. " a envoyer un payload qui n'est pas une table")
+        return
+    end
+    local centrifugesId = payload.centrifugesId
+    local key = payload.key
+    local i = payload.i
+
+    sleep(10)
+end)
 
 while true do
     yield()
