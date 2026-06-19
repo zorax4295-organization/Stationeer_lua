b0149b761b361ddcde14296621dc41827979a6fa
zorax4295 <108086156+zorax4295@users.noreply.github.com>
2026-06-19 19:16:50 +0200

    zorax4295/api
    mise a jour des snippet et de l'api avec scripted screen

---
+++ scripted_screen_API.lua
@@ -0,0 +1,93 @@
+---@meta -- permet d'avoir l'autocompletion sans que le fichier ai besoin d'etre ouvert
+
+---@class Icon
+
+
+
+ss = {}
+ss.direction = {
+    LeftToRight = {},
+    RightToLeft = {},
+    TopToBottom = {},
+    BottomToTop = {},
+}
+
+ss.ui = {}
+ss.ui.icons = {}
+
+---@type {Oxygen: Icon, Nitrogen: Icon, CarbonDioxide: Icon, Methane: Icon, Pollutant: Icon, Water: Icon, NitrousOxide: Icon, LiquidNitrogen: Icon, LiquidOxygen: Icon, LiquidMethane: Icon, Steam: Icon, LiquidCarbonDioxide: Icon, LiquidPollutant: Icon, LiquidNitrousOxide: Icon, Hydrogen: Icon, LiquidHydrogen: Icon, PollutedWater: Icon, Hydrazine: Icon, LiquidHydrazine: Icon, LiquidAlcohol: Icon, Helium: Icon, LiquidSodiumChloride: Icon, Silanol: Icon, LiquidSilanol: Icon, HydrochloricAcid: Icon, LiquidHydrochloricAcid: Icon, Ozone: Icon, LiquidOzone: Icon}
+ss.ui.icons.gas = {
+    Oxygen = {},
+    Nitrogen = {},
+    CarbonDioxide = {},
+    Methane = {},
+    Pollutant = {},
+    Water = {},
+    NitrousOxide = {},
+    LiquidNitrogen = {},
+    LiquidOxygen = {},
+    LiquidMethane = {},
+    Steam = {},
+    LiquidCarbonDioxide = {},
+    LiquidPollutant = {},
+    LiquidNitrousOxide = {},
+    Hydrogen = {},
+    LiquidHydrogen = {},
+    PollutedWater = {},
+    Hydrazine = {},
+    LiquidHydrazine = {},
+    LiquidAlcohol = {},
+    Helium = {},
+    LiquidSodiumChloride = {},
+    Silanol = {},
+    LiquidSilanol = {},
+    HydrochloricAcid = {},
+    LiquidHydrochloricAcid = {},
+    Ozone = {},
+    LiquidOzone = {},
+}
+---@type {None: Icon, Helmet: Icon, Suit: Icon, Back: Icon, GasFilter: Icon, GasCanister: Icon, Motherboard: Icon, Circuitboard: Icon, DataDisk: Icon, Organ: Icon, Ore: Icon, Plant: Icon, Uniform: Icon, Entity: Icon, Battery: Icon, Egg: Icon, Belt: Icon, Tool: Icon, Appliance: Icon, Ingot: Icon, Torpedo: Icon, Cartridge: Icon, AccessCard: Icon, Magazine: Icon, Circuit: Icon, Bottle: Icon, ProgrammableChip: Icon, Glasses: Icon, CreditCard: Icon, DirtCanister: Icon, SensorProcessingUnit: Icon, LiquidCanister: Icon, LiquidBottle: Icon, Wreckage: Icon, SoundCartridge: Icon, DrillHead: Icon, ScanningHead: Icon, Flare: Icon, SuitMod: Icon, Crate: Icon, NonePortables: Icon, RocketPayload: Icon}
+ss.ui.icons.slot = {
+    None = {},
+    Helmet = {},
+    Suit = {},
+    Back = {},
+    GasFilter = {},
+    GasCanister = {},
+    Motherboard = {},
+    Circuitboard = {},
+    DataDisk = {},
+    Organ = {},
+    Ore = {},
+    Plant = {},
+    Uniform = {},
+    Entity = {},
+    Battery = {},
+    Egg = {},
+    Belt = {},
+    Tool = {},
+    Appliance = {},
+    Ingot = {},
+    Torpedo = {},
+    Cartridge = {},
+    AccessCard = {},
+    Magazine = {},
+    Circuit = {},
+    Bottle = {},
+    ProgrammableChip = {},
+    Glasses = {},
+    CreditCard = {},
+    DirtCanister = {},
+    SensorProcessingUnit = {},
+    LiquidCanister = {},
+    LiquidBottle = {},
+    Wreckage = {},
+    SoundCartridge = {},
+    DrillHead = {},
+    ScanningHead = {},
+    Flare = {},
+    SuitMod = {},
+    Crate = {},
+    NonePortables = {},
+    RocketPayload = {},
+}
