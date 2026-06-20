ef1a5ecabdd3f349cf07daa87674d994eae9a6eb
zorax4295 <108086156+zorax4295@users.noreply.github.com>
2026-06-20 16:37:38 +0200

    Definition des première donné de l'os

---
+++ source/zorax4295/zorax4295_os.lua
@@ -0,0 +1,76 @@
+--Utilisation sur un programmable visor
+
+
+----------------------------
+-- import de la librairie
+----------------------------
+
+local system = require("system")
+
+
+
+-----------------------------------------------------
+-- Création des écran
+-----------------------------------------------------
+
+local currentUi = "setting"
+-- Déffinition des parametre de chaque écran
+local function createScreen(name)
+    local surface = ss.hud.surface(name)
+    return {
+        -- Rend accessible la surface pour utilisation ulterieur
+        surface = surface,
+        -- Affiche l'écran
+        set = function()
+            -- Retourne l'id de la page acctuel
+            currentUi = name
+            ss.hud.activate(name)
+        end,
+        -- Retire tout les élément afficher a l'écran
+        clear = function ()
+            surface:clear()            
+        end,
+    }
+end
+
+--Liste toute les page 
+local ui = {
+    setting = createScreen("setting"),
+}
+
+
+local size = ui.setting.surface:size()
+
+-- Déffinition de la taille de l'écran virtuelle
+local w = size.w
+local h = size.h
+print(system.log.time() .. "h " .. system.log.level("debug") .. " : w=" .. w .. " | h=" .. h)
+
+-----------------------------------------------------
+-- Initialisation des ecran
+-----------------------------------------------------
+
+ui.setting.clear()
+ui.setting.set()
+
+
+----------------------------
+-- Définition des appareil
+----------------------------
+
+
+----------------------------
+-- Définition des donnés
+----------------------------
+
+local LT = ic.enums.LogicType
+
+
+
+----------------------------
+-- Dessin de l'interface
+----------------------------
+
+while true do
+    yield()
+end
